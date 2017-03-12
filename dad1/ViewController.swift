574dc23724320fdac4835d61b6958bc0d2278b2a
Ian Jackson <Ian@Ians-MacBook-Air.local>
2017-03-12 15:27:36 +0100

    Merging from the Second Branch into Master

--- dad1/ViewController.swift
+++ dad1/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func sayHello(_ sender: Any) {
-        helloLabel.text = "Hello World from GITHub on Master Branch"
+        helloLabel.text = "Hello World from the second branch"
     }
     @IBOutlet weak var helloLabel: UILabel!
     override func viewDidLoad() {
